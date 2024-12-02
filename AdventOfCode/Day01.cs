2cb726c1fc453c86d9eb2b17c70603cc0825ff47
Farley <farleysr89@gmail.com>
2024-12-02 08:35:45 -0800

    Random updates

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,13 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
-    private List<int> _list1 = [];
-    private List<int> _list2 = [];
+    private readonly List<int> _list1 = [];
+    private readonly List<int> _list2 = [];
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
-        var lines = _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var input = File.ReadAllText(InputFilePath);
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
         foreach (var l in lines)
         {
             var entries = l.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray();
@@ -23,22 +22,13 @@ public class Day01 : BaseDay
         var sortedList1 = _list1.OrderBy(x => x).ToList();
         var sortedList2 = _list2.OrderBy(x => x).ToList();
         if(sortedList1.Count != sortedList2.Count) throw new Exception("Something Broke!");
-        var totalDiff = 0;
-        for (var i = 0; i < sortedList1.Count; i++)
-        {
-            totalDiff += Math.Abs(sortedList1[i] - sortedList2[i]);
-        }
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is " + totalDiff);
+        var totalDiff = sortedList1.Select((t, i) => Math.Abs(t - sortedList2[i])).Sum();
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is " + totalDiff);
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var simScore = 0;
-        foreach (var i in _list1)
-        {
-            var count = _list2.Count(x => x == i);
-            simScore += i * count;
-        }
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + simScore);
+        var simScore = (from i in _list1 let count = _list2.Count(x => x == i) select i * count).Sum();
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + simScore);
     }
 }
