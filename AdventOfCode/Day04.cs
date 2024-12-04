38e0319181e8a35aa2ea414a1039faaff27f5a31
Farley <33631912+farleysr89@users.noreply.github.com>
2024-12-03 23:36:47 -0800

    Day 4 part 2

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -28,14 +28,13 @@ public sealed class Day04 : BaseDay
                     if (j <= puzzle[i].Length - 3 && puzzle[i - 1][j + 1] == 'M' && puzzle[i - 2][j + 2] == 'A' &&
                         puzzle[i - 3][j + 3] == 'S') count++;
                 }
-                if (i <= puzzle.Length - 4)
-                {
-                    if (puzzle[i+1][j] == 'M' && puzzle[i+2][j] == 'A' && puzzle[i+3][j] == 'S') count++;
-                    if (j >= 3 && puzzle[i +1][j - 1] == 'M' && puzzle[i + 2][j - 2] == 'A' &&
-                        puzzle[i + 3][j - 3] == 'S') count++;
-                    if (j <= puzzle[i].Length - 3 && puzzle[i + 1][j + 1] == 'M' && puzzle[i + 2][j + 2] == 'A' &&
-                        puzzle[i + 3][j + 3] == 'S') count++;
-                }                
+
+                if (i > puzzle.Length - 4) continue;
+                if (puzzle[i+1][j] == 'M' && puzzle[i+2][j] == 'A' && puzzle[i+3][j] == 'S') count++;
+                if (j >= 3 && puzzle[i +1][j - 1] == 'M' && puzzle[i + 2][j - 2] == 'A' &&
+                    puzzle[i + 3][j - 3] == 'S') count++;
+                if (j <= puzzle[i].Length - 3 && puzzle[i + 1][j + 1] == 'M' && puzzle[i + 2][j + 2] == 'A' &&
+                    puzzle[i + 3][j + 3] == 'S') count++;
             }
         }
 
@@ -44,9 +43,21 @@ public sealed class Day04 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        var results = 0;
-        
-        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + results);
+        var count = 0;
+        var puzzle = _input.Split('\n');
+        for (var i = 1; i < puzzle.Length - 1; i++)
+        {
+            for (var j = 1; j < puzzle[i].Length - 2; j++)
+            {
+                if (puzzle[i][j] != 'A') continue;
+                if (((puzzle[i - 1][j - 1] == 'M' && puzzle[i + 1][j + 1] == 'S') ||
+                    (puzzle[i - 1][j - 1] == 'S' && puzzle[i + 1][j + 1] == 'M')) && 
+                    ((puzzle[i - 1][j + 1] == 'M' && puzzle[i + 1][j - 1] == 'S') || 
+                     (puzzle[i - 1][j + 1] == 'S' && puzzle[i + 1][j - 1] == 'M'))) count++;
+            }
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + count);
     }
 
 }
