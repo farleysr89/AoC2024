c41712c56952bb28f9f3e713f01d3414519e09de
Farley <33631912+farleysr89@users.noreply.github.com>
2024-12-05 23:23:23 -0800

    Day 6 part 1

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,143 @@
+﻿namespace AdventOfCode;
+
+public sealed class Day06 : BaseDay
+{
+    private readonly string _input;
+
+    public Day06()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var count = 0;
+        var validUpdates = 0;
+        var loc = (0,0);
+        var dir = ' ';
+        var map = _input.Split('\n');
+        var path = new bool[map.Length][];
+        for (var i = 0; i < path.Length; i++)
+        {
+            path[i] = new bool[map[0].Length];
+        }
+        var x = 0;
+        foreach (var line in map)
+        {
+            var y = 0;
+            foreach(var c in line)
+            {
+                if(c is '^' or '<' or '>' or 'v')
+                {
+                    loc = (x, y);
+                    dir = c;
+                    break;
+                }
+
+                y++;
+            }
+
+            if (dir != ' ') break;
+            x++;
+        }
+
+        while (loc.Item1 >= 0 && loc.Item1 < map.Length && loc.Item2 >= 0 && loc.Item2 < map[0].Length)
+        {
+            path[loc.Item1][loc.Item2] = true;
+            switch (dir)
+            {
+                case '^':
+                    if (loc.Item1 - 1 < 0)
+                    {
+                        loc.Item1--;
+                        continue;
+                    }
+                    if (map[loc.Item1 - 1][loc.Item2] == '#')
+                    {
+                        dir = '>';
+                        continue;
+                    }
+
+                    loc.Item1--;
+                    break;
+                case '<':
+                    if (loc.Item2 - 1 < 0)
+                    {
+                        loc.Item2--;
+                        continue;
+                    }
+                    if (map[loc.Item1][loc.Item2 - 1] == '#')
+                    {
+                        dir = '^';
+                        continue;
+                    }
+
+                    loc.Item2--;
+                    break;
+                case '>':
+                    if (loc.Item2 + 1 == map[0].Length)
+                    {
+                        loc.Item2++;
+                        continue;
+                    }
+                    if (map[loc.Item1][loc.Item2 + 1] == '#')
+                    {
+                        dir = 'v';
+                        continue;
+                    }
+
+                    loc.Item2++;
+                    break;
+                case 'v':
+                    if (loc.Item1 + 1 == map.Length)
+                    {
+                        loc.Item1++;
+                        continue;
+                    }
+                    if (map[loc.Item1 + 1][loc.Item2] == '#')
+                    {
+                        dir = '<';
+                        continue;
+                    }
+
+                    loc.Item1++;
+                    break;
+                default:
+                    throw new Exception("Invalid direction:" + dir);
+            }
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is " + path.Sum(l => l.Count(c => c)));
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var count = 0;
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + count);
+    }
+
+    private static int Reorder(List<(int, int)> rules, int[] report)
+    {
+        while (true)
+        {
+            var change = false;
+            for (var i = 0; i < report.Length - 1; i++)
+            {
+                for (var j = i + 1; j < report.Length; j++)
+                {
+                    var rule = rules.FirstOrDefault(r => r.Item1 == report[j] && r.Item2 == report[i]);
+                    if (rule == default) continue;
+                    change = true;
+                    report[i] = rule.Item1;
+                    report[j] = rule.Item2;
+                    break;
+                }
+                if(change) break;
+            }
+            if(!change) break;
+        }
+
+        return report[report.Length / 2];
+    }
+
+}
