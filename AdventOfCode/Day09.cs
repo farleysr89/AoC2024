99a3ca64c20f90f93eb12b0a7671fe7a63f35fcb
Farley <33631912+farleysr89@users.noreply.github.com>
2024-12-08 23:24:43 -0800

    Day 9 setup

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode;
+
+public sealed class Day09 : BaseDay
+{
+    private readonly string _input;
+
+    public Day09()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 is " + 0);
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 is " + 0);
+    }
+
+}
